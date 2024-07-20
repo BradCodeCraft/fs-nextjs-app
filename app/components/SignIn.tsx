49c4e4f2648f1981ed0b0c6520a388dec7400040
BradCodeCraft <bradcodecraft@gmail.com>
2024-07-20 11:57:44 -0400

    add sign in and sign up pages

---
+++ app/components/SignIn.tsx
@@ -0,0 +1,69 @@
+"use client";
+import React, { useState } from "react";
+import { LandingPageProps } from "../page";
+import { FaRegUser } from "react-icons/fa";
+import { RiLockPasswordLine } from "react-icons/ri";
+
+export default function SignIn(props: LandingPageProps) {
+  const { setHasAccount } = props;
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    throw new Error("Function not implemented.");
+  }
+
+  return (
+    <div className="w-1/3 min-h-80 py-5 px-10">
+      <h1 className="text-center text-4xl mb-2.5 text-white">
+        Welcome to BradCodeCraft
+      </h1>
+
+      <form
+        className="flex flex-col py-2 px-5"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <div className="flex w-full bg-slate-300 mb-2.5 rounded-md">
+          <div className="min-h-full px-5 flex items-center text-2xl">
+            <FaRegUser />
+          </div>
+          <div className="w-[1px] h-5 my-auto border border-black" />
+          <input
+            className="text-xl rounded-md py-2 px-5 w-full bg-transparent focus:outline-none"
+            name="username"
+            type="text"
+            value={username}
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div className="flex w-full bg-slate-300 rounded-md">
+          <div className="min-h-full px-5 flex items-center text-2xl">
+            <RiLockPasswordLine />
+          </div>
+          <div className="w-[1px] h-5 my-auto border border-black" />
+          <input
+            className="text-xl rounded-md py-2 px-5 w-full bg-transparent focus:outline-none"
+            name="password"
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex justify-end text-white">
+          <button onClick={() => setHasAccount(false)}>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            Don't have an account? Sign Up
+          </button>
+        </div>
+
+        <button className="bg-slate-100 rounded-xl text-xl py-1 mt-5">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
+}
---
+++ app/components/SignUp.tsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useState } from "react";
+import { LandingPageProps } from "../page";
+import { PiIdentificationBadge } from "react-icons/pi";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { FaRegUser } from "react-icons/fa";
+
+export default function SignUp(props: LandingPageProps) {
+  const { setHasAccount } = props;
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    throw new Error("Function not implemented.");
+  }
+
+  return (
+    <div className="w-1/3 min-h-80 py-5 px-10">
+      <h1 className="text-center text-4xl mb-2.5 text-white">
+        Welcome to BradCodeCraft
+      </h1>
+
+      <form
+        className="flex flex-col py-2 px-5"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <div className="flex w-full bg-slate-300 mb-2.5 rounded-md">
+          <div className="min-h-full px-5 flex items-center text-2xl">
+            <PiIdentificationBadge />
+          </div>
+          <div className="w-[1px] h-5 my-auto border border-black" />
+          <input
+            className="text-xl rounded-md py-2 px-5 w-full bg-transparent focus:outline-none"
+            name="firstName"
+            type="text"
+            value={firstName}
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+
+        <div className="flex w-full bg-slate-300 mb-2.5 rounded-md">
+          <div className="min-h-full px-5 flex items-center text-2xl">
+            <PiIdentificationBadge />
+          </div>
+          <div className="w-[1px] h-5 my-auto border border-black" />
+          <input
+            className="text-xl rounded-md py-2 px-5 w-full bg-transparent focus:outline-none"
+            name="lastName"
+            type="text"
+            value={lastName}
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+
+        <div className="flex w-full bg-slate-300 mb-2.5 rounded-md">
+          <div className="min-h-full px-5 flex items-center text-2xl">
+            <FaRegUser />
+          </div>
+          <div className="w-[1px] h-5 my-auto border border-black" />
+          <input
+            className="text-xl rounded-md py-2 px-5 w-full bg-transparent focus:outline-none"
+            name="username"
+            type="text"
+            value={username}
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div className="flex w-full bg-slate-300 rounded-md">
+          <div className="min-h-full px-5 flex items-center text-2xl">
+            <RiLockPasswordLine />
+          </div>
+          <div className="w-[1px] h-5 my-auto border border-black" />
+          <input
+            className="text-xl rounded-md py-2 px-5 w-full bg-transparent focus:outline-none"
+            name="password"
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex justify-end text-white">
+          <button onClick={() => setHasAccount(true)}>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            Already have an account? Sign In
+          </button>
+        </div>
+
+        <button className="bg-slate-100 rounded-xl text-xl py-1 mt-5">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+}
